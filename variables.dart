8428a0c8782576962c23eebd0a2a6763c9c4237f
Rana Habib <60252989+RanaHabib00@users.noreply.github.com>
2020-06-18 18:30:26 +0200

    Create variables.dart

---
+++ variables.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+const inactiveCard = Color(0xff1d1e33);
+const activeCard = Color(0xff111328);
+enum humanType{
+  male,
+  female,
+}
+
+final TextStyle myStyle =
+TextStyle(
+    fontSize: 18,
+    color: Color(0xff8d8e98)
+);
+
+final TextStyle sliders = TextStyle(fontSize: 50,fontWeight: FontWeight.w900);
+
+int heightValue=110;
+
+int weight=40;
+int age = 20;
+
+const ktitleStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle  = TextStyle(
+  color: Color(0xff24d876),
+  fontSize: 22,
+  fontWeight: FontWeight.bold,
+);
